c1bab6d8a5678b665eb80d8d285e9529069db4b8
Alberto Rosales <alberto_rosales@avansys.com.mx>
2020-09-09 18:34:19 -0500

    se agregaron plantillas

--- web/index.php
+++ web/index.php
@@ -21,5 +21,25 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
+$app->get('/barra', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('barra.twig');
+});
+$app->get('/contenido', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('contenido.twig');
+});
+$app->get('/test', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('test.twig');
+});
+$app->get('/tops', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('tops.twig');
+});
+$app->get('/webcam', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('webcam.twig');
+});
 
 $app->run();
