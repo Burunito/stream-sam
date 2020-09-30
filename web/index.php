1f26a4e6d93c57a7d08de242794f8a11e441f658
Alberto Rosales <alberto_rosales@avansys.com.mx>
2020-09-29 23:22:48 -0500

    se agrego overlay de evangelion

--- web/index.php
+++ web/index.php
@@ -37,6 +37,20 @@ $app->get('/fondos/spider-web', function() use($app) {
   return $app['twig']->render('fondos/spider-web.twig');
 });
 
+$app->get('/fondos/cotorrisa', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('fondos/cotorrisa.twig');
+});
+
+$app->get('/fondos/evangelion', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('fondos/evangelion.twig');
+});
+
+$app->get('/fondos/naruto', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('fondos/naruto.twig');
+});
 //barras
 $app->get('/barras/redes-rosa-kawai', function() use($app) {
   $app['monolog']->addDebug('logging output.');
@@ -51,6 +65,18 @@ $app->get('/barras/redes-morado-spiderweb', function() use($app) {
   return $app['twig']->render('barras/redes-morado-spiderweb.twig');
 });
 
+$app->get('/barras/redes-cotorrisa', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('barras/redes-cotorrisa.twig');
+});
+$app->get('/barras/redes-naruto', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('barras/redes-naruto.twig');
+});
+$app->get('/barras/redes-evangelion', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('barras/redes-evangelion.twig');
+});
 //TOPS
 $app->get('/tops/tops-rosa-kawai', function() use($app) {
   $app['monolog']->addDebug('logging output.');
@@ -60,7 +86,18 @@ $app->get('/tops/tops-morado-spider', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('tops/tops-morado-spider.twig');
 });
-
+$app->get('/tops/tops-cotorrisa', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('tops/tops-cotorrisa.twig');
+});
+$app->get('/tops/tops-naruto', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('tops/tops-naruto.twig');
+});
+$app->get('/tops/tops-evangelion', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('tops/tops-evangelion.twig');
+});
 //MARCOS
 $app->get('/marcos/webcam-rosa-kawai', function() use($app) {
   $app['monolog']->addDebug('logging output.');
@@ -87,4 +124,16 @@ $app->get('/marcos/contenido-morado-halloween', function() use($app) {
   return $app['twig']->render('marcos/contenido-morado-halloween.twig');
 });
 
+$app->get('/marcos/contenido-cotorrisa', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('marcos/contenido-cotorrisa.twig');
+});
+$app->get('/marcos/contenido-naruto', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('marcos/contenido-naruto.twig');
+});
+$app->get('/marcos/contenido-evangelion', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('marcos/contenido-evangelion.twig');
+});
 $app->run();
